32bdb81bbbf4d31e4a6a0eaa648f5c6ad7412977
thms <ThmsP@users.noreply.github.com>
2020-10-30 22:59:49 +0100

    Publish

---
+++ sw.js
@@ -0,0 +1,129 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "android-chrome-192x192.0fcebb82.png",
+    "revision": "71c32dc8b4da4840d61a1796a2bc230b"
+  },
+  {
+    "url": "android-chrome-512x512.ae40f73d.png",
+    "revision": "8bf8012c4f4ae251d660b4739171b7ed"
+  },
+  {
+    "url": "apple-touch-icon-120x120-precomposed.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-120x120.044c6f3f.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-precomposed.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "apple-touch-icon.fdeabd24.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "certificate.d1673940.pdf",
+    "revision": "624e7a3f5560db68d93eda0129cc2922"
+  },
+  {
+    "url": "confidentialite.0cad1f6d.css",
+    "revision": "cc96a523e05a5ec85aca2b651bf07d61"
+  },
+  {
+    "url": "confidentialite.9c763be1.js",
+    "revision": "bd02eaf5385a95b538b0f147fd978c89"
+  },
+  {
+    "url": "confidentialite.html",
+    "revision": "25f70862c7800d85b4fbedb61a65c168"
+  },
+  {
+    "url": "favicon-16x16.a4687270.png",
+    "revision": "652605ae0182d51781ab0be639ad2bda"
+  },
+  {
+    "url": "favicon-32x32.623384d0.png",
+    "revision": "674c9c4ef1e4c7ea9de1218ee0bfd0cf"
+  },
+  {
+    "url": "index.html",
+    "revision": "056fc61f0ce2d94e70cba0b31ca43ca4"
+  },
+  {
+    "url": "logo_dnum_dark.0fe33c5b.svg",
+    "revision": "da8bdc57d4f231585216c53da752d00a"
+  },
+  {
+    "url": "logo_dnum.19ebc682.svg",
+    "revision": "3a41bfa41e4671414da29db168c37d66"
+  },
+  {
+    "url": "main.0cad1f6d.css",
+    "revision": "4ff4d0351a40e3e74cd8cec4fd7e1624"
+  },
+  {
+    "url": "main.c7be006b.js",
+    "revision": "768b0e502533567bb2a32ae96508c367"
+  },
+  {
+    "url": "marianne-bold-webfont.1505950c.woff2",
+    "revision": "e67f6cefe32cc39f909e605c8d6337a9"
+  },
+  {
+    "url": "marianne-bold-webfont.7424dbde.woff",
+    "revision": "0bcc99dd4adfb78e11098fedfe531cbb"
+  },
+  {
+    "url": "marianne-regular-webfont.0a959359.woff",
+    "revision": "89f4f2326c77429e98693cf83703face"
+  },
+  {
+    "url": "marianne-regular-webfont.daa94941.woff2",
+    "revision": "d2c09e5f58d8360f541e2a8726c33587"
+  },
+  {
+    "url": "MIN_Interieur_RVB_dark.0e5ee525.svg",
+    "revision": "345794cee228a40837ab654184cd2c96"
+  },
+  {
+    "url": "MIN_Interieur_RVB.124e26ea.svg",
+    "revision": "6823b6d87f43d208b17ff81e178f9ae9"
+  },
+  {
+    "url": "safari-pinned-tab.1551797e.svg",
+    "revision": "f53452e6ac8760f12bab91672e91d39b"
+  },
+  {
+    "url": "./",
+    "revision": "8f78ece5505385293f978855dc740269"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
